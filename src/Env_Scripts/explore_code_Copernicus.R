e39233e18ac9fc5b17ace8dcee017593d896cc20
Camille <rumberger.c@northeastern.edu>
2025-08-21 11:08:18 -0400

    update Marlene's script for quantiles

--- src/Env_Scripts/explore_code_Copernicus.R
+++ src/Env_Scripts/explore_code_Copernicus.R
@@ -2,43 +2,46 @@ install.packages("R.matlab")
 library(R.matlab)
 
 ###salinity
-data <- readMat("SalinityDaily.mat")
+sal <- readMat("data/EnvDat/SalinityDaily.mat")
 #rows are the 39 sites in the same order that I got from you, the columns are the data for the 92 days with 1 June in the first column, and then there are 11 slices, one for each year, where the first slice is year 2011.
 
-head(data)
-summary(data)
-str(data)
+head(sal)
+summary(sal)
+str(sal)
 
-mean(data$SalinityDaily)
+mean(sal$SalinityDaily)
 9.16166
 
 # Extracting the slice for the first year (2011)
 #year_2011_slice <- data$SalinityDaily[, , 1]
 
 # Calculate the mean for each site over all years, regardless of the day
-mean_for_sites <- apply(data$SalinityDaily, MARGIN = 1, mean)
+mean_for_sites <- apply(sal$SalinityDaily, MARGIN = 1, mean)
 #margin=1 gives mean for each site regardless of day
-min_sal_for_sites <- apply(data$SalinityDaily, MARGIN = 1, min)
-sal_percentile_10_for_sites <- apply(data$SalinityDaily, MARGIN = 1, function(x) quantile(x, 0.1))
-sal_percentile_1_for_sites <- apply(data$SalinityDaily, MARGIN = 1, function(x) quantile(x, 0.01))
+min_sal_for_sites <- apply(sal$SalinityDaily, MARGIN = 1, min)
+sal_percentile_10_for_sites <- apply(sal$SalinityDaily, MARGIN = 1, function(x) quantile(x, 0.1))
+sal_percentile_1_for_sites <- apply(sal$SalinityDaily, MARGIN = 1, function(x) quantile(x, 0.01))
+sal_percentile_90_for_sites <- apply(sal$SalinityDaily, MARGIN = 1, function(x) quantile(x, 0.9))
 
 ###temp
-data <- readMat("TempDaily.mat")
+temp <- readMat("data/EnvDat/TempDaily.mat")
 #rows are the 39 sites in the same order that I got from you, the columns are the data for the 92 days with 1 June in the first column, and then there are 11 slices, one for each year, where the first slice is year 2011.
 
-head(data)
-summary(data)
-str(data)
+head(temp)
+summary(temp)
+str(temp)
 
 
 # Calculate the mean for each site over all years, regardless of the day
-T_mean_for_sites <- apply(data$TempDaily, MARGIN = 1, mean)
+T_mean_for_sites <- apply(temp$TempDaily, MARGIN = 1, mean)
 #margin=1 gives mean for each site regardless of day
-T_max_for_sites <- apply(data$TempDaily, MARGIN = 1, max)
+T_max_for_sites <- apply(temp$TempDaily, MARGIN = 1, max)
 
 # Calculate the 90th percentile for each site
-T_percentile_90_for_sites <- apply(data$TempDaily, MARGIN = 1, function(x) quantile(x, 0.9))
-T_percentile_99_for_sites <- apply(data$TempDaily, MARGIN = 1, function(x) quantile(x, 0.99))
+T_percentile_90_for_sites <- apply(temp$TempDaily, MARGIN = 1, function(x) quantile(x, 0.9))
+T_percentile_99_for_sites <- apply(temp$TempDaily, MARGIN = 1, function(x) quantile(x, 0.99))
+T_percentile_10_for_sites <- apply(temp$TempDaily, MARGIN = 1, function(x) quantile(x, 0.1))
+
 
 str(data)
 
@@ -66,7 +69,7 @@ max_temp_for_5_days <- function(site_data) {
 }
 
 # Apply the function to each site
-max_temp_for_5_days_for_sites <- apply(data$TempDaily, MARGIN = 1, max_temp_for_5_days)
+max_temp_for_5_days_for_sites <- apply(temp$TempDaily, MARGIN = 1, max_temp_for_5_days)
 
 
 
